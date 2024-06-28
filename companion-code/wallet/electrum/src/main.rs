fcbec7088e92cbe97e7fbdf54c9c3772a0fcd6e6
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-06-28 10:36:27 -0400

    docs: update bdk_wallet electrum page to alpha 13

--- companion-code/wallet/electrum/src/main.rs
+++ companion-code/wallet/electrum/src/main.rs
@@ -1,36 +1,3 @@
-# Working with Electrum
-
-!!! tip
-    This page is up-to-date with version `1.0.0-alpha.13` of bdk.
-
-### 1. Create a new Rust project
-```shell
-cargo init electrumexample
-cd electrumexample
-```
-
-### 2. Add required bdk dependencies to your Cargo.toml file
-```toml
-[package]
-name = "electrumexample"
-version = "0.1.0"
-edition = "2021"
-
-[dependencies]
-bdk_wallet = { version = "=1.0.0-alpha.13" }
-bdk_sqlite = { version = "=0.2.0" }
-bdk_electrum = { version = "=0.15.0" }
-```
-
-### 3. Create your descriptors
-Refer to the [Working with Descriptors](../descriptors.md) page for information on how to generate descriptors. This page will assume you are working on signet with the following BIP86 descriptors:
-```txt
-tr(tprv8ZgxMBicQKsPewab4KfjNu6p9Q5XAPokRpK9zrPGoJS7H6CqnxuKJX6zPBDj2Q43tfmVBRTpQMBSg8AhqBDdNEsBC14kMXiZj2tPWv5wHAE/86'/1'/0'/0/*)
-tr(tprv8ZgxMBicQKsPewab4KfjNu6p9Q5XAPokRpK9zrPGoJS7H6CqnxuKJX6zPBDj2Q43tfmVBRTpQMBSg8AhqBDdNEsBC14kMXiZj2tPWv5wHAE/86'/1'/0'/1/*)
-```
-
-### Create and sync the wallet
-```rust
 use bdk_wallet::wallet::AddressInfo;
 use bdk_wallet::KeychainKind;
 use bdk_wallet::bitcoin::Network;
@@ -65,12 +32,22 @@ fn main() -> () {
     let mut update = client
         .full_scan(full_scan_request, STOP_GAP, BATCH_SIZE, true).unwrap()
         .with_confirmation_time_height_anchor(&client).unwrap();
+    // println!("Full scan result: {:?}", full_scan_result);
 
     let now = std::time::UNIX_EPOCH.elapsed().unwrap().as_secs();
     let _ = update.graph_update.update_last_seen_unconfirmed(now);
 
     wallet.apply_update(update).unwrap();
     let balance = wallet.balance();
-    println!("Wallet balance: {} sats", balance.total().to_sat());
+    println!("Wallet balance: {} sat", balance.total().to_sat());
 }
-```
+
+fn print_page_link(link: &str) -> () {
+    println!();
+    println!("+------------------------------------------------------------------------------------------+");
+    println!("|                                                                                          |");
+    println!("| Companion code for https://bitcoindevkit.github.io/book-of-bdk/cookbook/wallet/{} |", link);
+    println!("|                                                                                          |");
+    println!("+------------------------------------------------------------------------------------------+");
+    println!();
+}
