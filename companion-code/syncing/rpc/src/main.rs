a637c33d08a981c685e7ff15838d2df85b5687db
valued mammal <valuedmammal@protonmail.com>
2024-08-02 12:53:39 -0500

    feat: update syncing/rpc code to bdk 1.0.0-beta.1

--- companion-code/syncing/rpc/src/main.rs
+++ companion-code/syncing/rpc/src/main.rs
@@ -1,34 +1,36 @@
-use bdk_wallet::wallet::{AddressInfo, Balance};
-use bdk_wallet::bitcoin::{Block, Network, Transaction};
-use bdk_wallet::{KeychainKind, Wallet};
-use bdk_wallet::chain::local_chain::CheckPoint;
 use bdk_bitcoind_rpc::bitcoincore_rpc::{Auth, Client, RpcApi};
-use bdk_bitcoind_rpc::{BlockEvent, Emitter};
+use bdk_bitcoind_rpc::Emitter;
+use bdk_wallet::bitcoin::{Network, Transaction};
+use bdk_wallet::chain::local_chain::CheckPoint;
+use bdk_wallet::{AddressInfo, Balance, KeychainKind, Wallet};
 
-const COOKIE_FILE_PATH: &str = "<path_to_your_regtest_bitcoin_core_cookie_file>/.cookie";
+const COOKIE_FILE_PATH: &str = "<path_to_your_regtest_bitcoin_core_data_dir>/.cookie";
 const EXTERNAL_DESCRIPTOR: &str = "tr(tprv8ZgxMBicQKsPdrjwWCyXqqJ4YqcyG4DmKtjjsRt29v1PtD3r3PuFJAjWytzcvSTKnZAGAkPSmnrdnuHWxCAwy3i1iPhrtKAfXRH7dVCNGp6/86'/1'/0'/0/*)#g9xn7wf9";
 const INTERNAL_DESCRIPTOR: &str = "tr(tprv8ZgxMBicQKsPdrjwWCyXqqJ4YqcyG4DmKtjjsRt29v1PtD3r3PuFJAjWytzcvSTKnZAGAkPSmnrdnuHWxCAwy3i1iPhrtKAfXRH7dVCNGp6/86'/1'/0'/1/*)#e3rjrmea";
 
 fn main() -> () {
     print_page_link("rpc/");
 
-    let mut wallet: Wallet = Wallet::new(
-        EXTERNAL_DESCRIPTOR,
-        INTERNAL_DESCRIPTOR,
-        Network::Regtest,
-    ).unwrap();
+    let mut wallet: Wallet = Wallet::create(EXTERNAL_DESCRIPTOR, INTERNAL_DESCRIPTOR)
+        .network(Network::Regtest)
+        .create_wallet_no_persist()
+        .unwrap();
 
     let balance: Balance = wallet.balance();
     println!("Wallet balance before syncing: {}", balance.total());
 
     let address: AddressInfo = wallet.reveal_next_address(KeychainKind::External);
-    println!("Generated address {} at index {}", address.address, address.index);
+    println!(
+        "Generated address {} at index {}",
+        address.address, address.index
+    );
 
     let rpc_client: Client = Client::new(
         "http://127.0.0.1:18443",
         // Auth::UserPass("__cookie__".to_string(), "cookievalue".to_string())
-        Auth::CookieFile(COOKIE_FILE_PATH.into())
-    ).unwrap();
+        Auth::CookieFile(COOKIE_FILE_PATH.into()),
+    )
+    .unwrap();
 
     let blockchain_info = rpc_client.get_blockchain_info().unwrap();
     println!(
@@ -46,16 +48,12 @@ fn main() -> () {
     let mut emitter = Emitter::new(&rpc_client, wallet_tip.clone(), wallet_tip.height());
 
     println!("Syncing blocks...");
-    loop {
-        let block_event: Option<BlockEvent<Block>> = emitter.next_block().unwrap();
-        let block = if block_event.is_none() {
-            break;
-        } else {
-            block_event.unwrap()
-        };
+    while let Some(block) = emitter.next_block().unwrap() {
         print!("{} ", block.block_height());
-
-        wallet.apply_block_connected_to(&block.block, block.block_height(), block.connected_to()).unwrap();
+    
+        wallet
+            .apply_block_connected_to(&block.block, block.block_height(), block.connected_to())
+            .unwrap();
     }
     println!();
 
