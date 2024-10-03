5e9c7648520e1ccecd7da191a4a7cd34bc3898b3
River Kanies <river.kanies@gmail.com>
2024-10-03 09:34:56 -0500

    code: persist selected language

--- docs/assets/js/link-tabs.js
+++ docs/assets/js/link-tabs.js
@@ -1,18 +1,23 @@
+const selectLanguage = (language) => {
+  const labels = document.querySelectorAll('.tabbed-set > label, .tabbed-alternate > .tabbed-labels > label')
+  for (const label of labels) {
+    const thisLabelContent = label.childNodes[0].innerHTML ? label.childNodes[0].innerHTML : label.innerHTML
+
+    if (thisLabelContent === language) {
+      document.querySelector(`input[id=${label.getAttribute('for')}]`).checked = true
+    }
+  }
+}
+
 const tabSync = () => {
   const tabs = document.querySelectorAll(".tabbed-set > input")
   for (const tab of tabs) {
     tab.addEventListener("click", () => {
       const current = document.querySelector(`label[for=${tab.id}]`)
       const pos = current.getBoundingClientRect().top
-      const labelContent = current.childNodes[0].innerHTML ? current.childNodes[0].innerHTML : current.innerHTML
-      const labels = document.querySelectorAll('.tabbed-set > label, .tabbed-alternate > .tabbed-labels > label')
-      for (const label of labels) {
-        const thisLabelContent = label.childNodes[0].innerHTML ? label.childNodes[0].innerHTML : label.innerHTML
-
-        if (thisLabelContent === labelContent) {
-          document.querySelector(`input[id=${label.getAttribute('for')}]`).checked = true
-        }
-      }
+      const language = current.childNodes[0].innerHTML ? current.childNodes[0].innerHTML : current.innerHTML
+      localStorage.setItem("language", language)
+      selectLanguage(language)
 
       // Preserve scroll position
       const delta = (current.getBoundingClientRect().top) - pos
@@ -20,6 +25,9 @@ const tabSync = () => {
     })
   }
 }
+
 window.addEventListener("load", (event) => {
   tabSync()
+  selectedLanguage = localStorage.getItem("language")
+  if (selectedLanguage && selectedLanguage != "") selectLanguage(selectedLanguage)
 });
