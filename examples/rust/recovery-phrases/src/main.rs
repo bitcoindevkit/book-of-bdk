f16c5b4c04fad93e4f289547be9072d508f0f003
thunderbiscuit <thunderbiscuit@protonmail.com>
2025-10-02 16:13:23 -0400

    examples: bump recovery phrases example to bdk_wallet 2.2

--- examples/rust/recovery-phrases/src/main.rs
+++ examples/rust/recovery-phrases/src/main.rs
@@ -4,15 +4,16 @@ use bdk_wallet::keys::bip39::Mnemonic;
 use bdk_wallet::template::{Bip86, DescriptorTemplate};
 use bdk_wallet::KeychainKind;
 
-const RECOVERY_PHRASE: &str = "[your 12 word seed phrase here ...]";
-// const RECOVERY_PHRASE: &str = "holiday marble tide globe license stumble rescue antenna monitor sea half sauce"; // example
-
 fn main() {
-    let mnemonic = Mnemonic::parse(RECOVERY_PHRASE).expect("Invalid seed! Be sure to replace the value of RECOVERY_PHRASE with your own 12 word seed phrase.");
+    println!("\n-------- Book of BDK Recovery Phrases Example ------------------------");
+
+    // --8<-- [start:main]
+    let recovery_phrase: &str = "awesome awesome awesome awesome awesome awesome awesome awesome awesome awesome awesome awesome";
+    let mnemonic = Mnemonic::parse(recovery_phrase).expect("Invalid seed! Be sure to replace the value of RECOVERY_PHRASE with your own 12 word seed phrase.");
     let seed = mnemonic.to_seed("");
-    let xprv: Xpriv =
-        Xpriv::new_master(Network::Signet, &seed).expect("Failed to create master key");
-    println!("Generated Master Private Key:\n{xprv}\nWarning: be very careful with seeds and private keys when using MainNet! We are logging these values for convenience only because this is an example on SigNet.\n");
+    let xprv: Xpriv = Xpriv::new_master(Network::Signet, &seed).expect("Failed to create master key");
+
+    println!("# Master Private Key\n{xprv}\nWarning: be very careful with seeds and private keys when using MainNet! We are logging these values for convenience and demonstration purposes only.\n");
 
     let (descriptor, key_map, _) = Bip86(xprv, KeychainKind::External)
         .build(Network::Signet)
@@ -21,12 +22,13 @@ fn main() {
     let (change_descriptor, change_key_map, _) = Bip86(xprv, KeychainKind::Internal)
         .build(Network::Signet)
         .expect("Failed to build internal descriptor");
+    // --8<-- [end:main]
 
     println!(
-        "----------------  Descriptors  ------------------------------\nPrivate Key, External:\n{:?}\nPrivate Key, Internal:\n{:?}\nPublic Key, External:\n{:?}\nPublic Key, Internal:\n{:?}\n",
-        descriptor.to_string_with_secret(&key_map), // privkey
+        "Private Key (External)\n{:?}\n\nPrivate Key (Internal)\n{:?}\n\nPublic Key (External)\n{:?}\n\nPublic Key (Internal)\n{:?}\n",
+        descriptor.to_string_with_secret(&key_map),
         change_descriptor.to_string_with_secret(&change_key_map),
-        descriptor.to_string(), // pubkey
+        descriptor.to_string(),
         change_descriptor.to_string()
     );
 }
