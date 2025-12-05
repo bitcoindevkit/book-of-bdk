7d39847e0dec37dd39971e876d985ccf095cd14e
thunderbiscuit <thunderbiscuit@protonmail.com>
2025-12-05 08:59:54 -0500

    chore: add deploy script for production website

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# This script will build the website and push it to the `prod` branch of the repo,
+# publishing it automatically to https://bookofbdk.com.
+
+rm -rf ./site/
+mkdocs build
+cd site
+git init .
+git switch --create prod
+git add .
+git commit --message "Deploy $(date +"%Y-%m-%d")"
+git remote add upstream git@github.com:bitcoindevkit/book-of-bdk.git
+git push upstream prod --force
+cd ..
